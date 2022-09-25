084f7347998b1c8149ad00d2f55e77322d8d777e
Mohammad saliq <64719725+darsaliq00@users.noreply.github.com>
2022-09-25 09:41:54 +0530

    Create basic.py

---
+++ Python/basic.py
@@ -0,0 +1,18 @@
+var1 = "1 "
+var2 = "12"
+var3 = 4
+var4 = 45
+var5 = 7
+print(int(var1) + int(var2))
+print(10 * "Hello world\n")
+print("enter a number")
+var6 = input()
+print ("enter second number")
+var7 = input()
+print("the sum is")
+print(int(var6) + int(var7))
+# this is comment
+'This will be ignored by Python'
+""" Python program to demonstrate
+ multiline comments"""
+print("Multiline comments")
