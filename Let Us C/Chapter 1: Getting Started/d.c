955aa1e6b46c449564dfbd327a1f94d20f7758d3
Mohammad saliq <64719725+darsaliq00@users.noreply.github.com>
2022-10-27 19:48:02 +0530

    Update d.c

--- Let Us C/Chapter 1: Getting Started/d.c
+++ Let Us C/Chapter 1: Getting Started/d.c
@@ -1,1 +1,12 @@
-
+/*Temperature of a city in Fahrenheit degrees is input through the keyboard. 
+Write a program to convert this temperature into Centigrade degrees. */
+#include<stdio.h>
+main( )
+{
+            float fahrenheit,degree_celsius;
+            printf("enter the temperature in fahrenheit: ");
+            scanf("%f",&fahrenheit);
+            degree_celsius=((fahrenheit-32)*5)/9;
+            printf("fehrenheit to celsius=%f\n",degree_celsius);
+}
+fahrenheit=(degree_celsius*9)/5+32;
