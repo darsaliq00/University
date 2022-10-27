d53b51f84707068db16a96715a552ad503178ed3
Mohammad saliq <64719725+darsaliq00@users.noreply.github.com>
2022-10-27 19:40:28 +0530

    Update program a.c

--- Let Us C/Chapter 1: Getting Started/program a.c
+++ Let Us C/Chapter 1: Getting Started/program a.c
@@ -1,3 +1,6 @@
+/* Ramesh’s basic salary is input through the keyboard.
+His dearness allowance is 40% of basic salary, and house rent allowance is 20% of basic salary.
+Write a program to calculate his gross salary.*/
 #include<stdio.h>
 main( )
 {
