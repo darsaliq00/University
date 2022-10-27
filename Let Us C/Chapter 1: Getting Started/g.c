a043b8cc2f08f88c9bd1dfd001535eafe314505b
Mohammad saliq <64719725+darsaliq00@users.noreply.github.com>
2022-10-27 19:54:22 +0530

    Create g.c

---
+++ Let Us C/Chapter 1: Getting Started/g.c
@@ -0,0 +1,25 @@
+/*If a five-digit number is input through the keyboard, 
+write a program to calculate the sum of its digits.*/
+#include<stdio.h>
+int main()
+{
+    int number, sum=0, i, n, count=0, num;
+    printf("Enter a Number: "); //n digits number as input
+    scanf("%d", &number);
+    num = number;
+    while(num!=0)
+    {
+        num /= 10;
+        count += 1;
+    }
+    for(i=0;i<count;i++)    //i=0 to i=count-1 (length(number))
+    {
+        n = number % 10;
+        number = number / 10;
+        sum = sum + n;
+    }
+
+    printf("Sum: %d", sum);
+    return 0;
+
+}
