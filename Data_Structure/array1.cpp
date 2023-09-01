0fdf36bacc009c72493931ec9d1ef974f909da1f
darsaliq00 <darsaliq00@gmail.com>
2023-09-01 11:57:50 +0530

    Initial commit

---
+++ Data_Structure/array1.cpp
@@ -0,0 +1,13 @@
+// basics
+#include<iostream>
+using namespace std;
+
+int main(){
+    int array[6]={3,5,6,7,5,9};
+
+    for(int i=0;i<=5;i++){
+        cout<<array[i];
+    }
+
+    return 0;
+}
