d19722113b5562edf7252f2f6ca3da34a9d5d3ce
Mohammad saliq <64719725+darsaliq00@users.noreply.github.com>
2023-12-05 10:21:52 +0530

    Create Quick sort_output.c

---
+++ Design and analysis of Algorithm lab/Quick sort_output.c
@@ -0,0 +1,7 @@
+When the above program is executed, it produces the following output −
+
+How many elements are u going to enter?: 10
+
+Enter 10 elements: 2 3 5 7 1 9 3 8 0 4
+
+Order of Sorted elements: 0 1 2 3 3 4 5 7 8 9
